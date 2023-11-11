6b525da8008532017e53984ca6031db436d0d07a
Rosa <kosolrosa@gmail.com>
2023-11-12 07:59:28 +1100

    Created online ordering add to cart elements

--- script.js
+++ script.js
@@ -47,65 +47,75 @@ $(document).ready(function(){
   })
 })
 
-// Multiple card carousel
-const wrapper = document.querySelector(".wrapper");
-const carousel = document.querySelector(".carousel-multiple");
-var firstCardWidth = carousel.querySelector(".card").offsetWidth;
-const arrowBtns = document.querySelectorAll(".wrapper i");
-const carouselChildrens = [...carousel.children];
-let isDragging = false, startX, startScrollLeft;
-// Get the number of cards that can fit in the carousel at once
-let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
-// Insert copies of the last few cards to beginning of carousel for infinite scrolling
-carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
-    carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
-});
-// Insert copies of the first few cards to end of carousel for infinite scrolling
-carouselChildrens.slice(0, cardPerView).forEach(card => {
-    carousel.insertAdjacentHTML("beforeend", card.outerHTML);
-});
-// Scroll the carousel at appropriate postition to hide first few duplicate cards on Firefox
-carousel.classList.add("no-transition");
-carousel.scrollLeft = carousel.offsetWidth;
-carousel.classList.remove("no-transition");
-// Add event listeners for the arrow buttons to scroll the carousel left and right
-arrowBtns.forEach(btn => {
-    btn.addEventListener("click", () => {
-        carousel.scrollLeft += btn.id == "left" ? -firstCardWidth : firstCardWidth;
-    });
-});
-const dragStart = (e) => {
-    isDragging = true;
-    carousel.classList.add("dragging");
-    // Records the initial cursor and scroll position of the carousel
-    startX = e.pageX;
-    startScrollLeft = carousel.scrollLeft;
-}
-const dragging = (e) => {
-    if(!isDragging) return; // if isDragging is false return from here
-    // Updates the scroll position of the carousel based on the cursor movement
-    carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
-}
-const dragStop = () => {
-    isDragging = false;
-    carousel.classList.remove("dragging");
-}
-const infiniteScroll = () => {
-    // If the carousel is at the beginning, scroll to the end
-    if(carousel.scrollLeft === 0) {
-        carousel.classList.add("no-transition");
-        carousel.scrollLeft = carousel.scrollWidth - (2 * carousel.offsetWidth);
-        carousel.classList.remove("no-transition");
-    }
-    // If the carousel is at the end, scroll to the beginning
-    else if(Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth) {
-        carousel.classList.add("no-transition");
-        carousel.scrollLeft = carousel.offsetWidth;
-        carousel.classList.remove("no-transition");
-    }
-}
+// 
+const product = [
+  {
+    id: 0,
+    image: 'assets/img/kfc.jpg',
+    title: 'Korean Fried Chicken',
+    price: 14.99,
+  },
+  {
+    id: 1,
+    image: 'assets/img/bulgogi.jpg',
+    title: 'Korean Fried Chicken',
+    price: 14.99,
+  },
+  {
+    id: 2,
+    image: 'assets/img/bibimbap.jpg',
+    title: 'Korean Fried Chicken',
+    price: 14.99,
+  },
+  {
+    id: 3,
+    image: 'assets/img/tteokbokki.jpg',
+    title: 'Korean Fried Chicken',
+    price: 14.99,
+  },
+  {
+    id: 4,
+    image: 'assets/img/kfc.jpg',
+    title: 'Korean Fried Chicken',
+    price: 14.99,
+  },
+  {
+    id: 5,
+    image: 'assets/img/kfc.jpg',
+    title: 'Korean Fried Chicken',
+    price: 14.99,
+  },
+  {
+    id: 6,
+    image: 'asset/img/kfc.jpg',
+    title: 'Korean Fried Chicken',
+    price: 14.99,
+  },
+  {
+    id: 7,
+    image: 'asset/img/kfc.jpg',
+    title: 'Korean Fried Chicken',
+    price: 14.99,
+  },
+];
 
-carousel.addEventListener("mousedown", dragStart);
-carousel.addEventListener("mousemove", dragging);
-document.addEventListener("mouseup", dragStop);
-carousel.addEventListener("scroll", infiniteScroll);
+const categories = [...new Set(product.map((item)=>
+  { return item}))]
+  let i = 0;
+
+document.getElementById('root').innerHTML = categories.map((item)=>
+{
+  var {image, title, price} = item;
+  return(
+    `<div class='box'>
+      <div class='img-box'>
+        <img class='images' src=${image}></img>
+        </div>
+    <div class='bottom'>
+    <h2>${title}</h2>
+    <p>$ ${price}.00</p>`+
+    "<button onclick='addtocart("+(i++)+")'>Add to cart</button>"+
+    `</div>
+    </div>`
+  )
+}).join('')
