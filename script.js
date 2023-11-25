24fd33dd1f2d194bed6128b600aa95e645a2a7b7
Rosa <kosolrosa@gmail.com>
2023-11-25 18:10:55 +1100

    Update script.js

--- script.js
+++ script.js
@@ -98,13 +98,31 @@ function sendMail() {
       alert("Your catering enquiry has been sent succcessfully!");
     })
   .catch(err=>console.log(err));
+}
+
+const form  = document.querySelector('form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
 
   const captchaResponse = grecaptcha.getResponse();
 
   if (!captchaResponse.length > 0) {
     throw new Error("Captcha not complete!");
   }
-}
+
+  const fd = new FormData(e.target);
+  const params = new URLSearchParams(fd);
+
+  fetch('http://httpbin.org/post', {
+    method: "POST",
+    body: params,
+  })
+
+  .then(res => res.json())
+  .then(data => console.log(data))
+  .catch(err => console.error(err))
+})
 
 
 // Shopping cart order form
