e3b3af1acd96637268f42b42c751c72a21f5617c
Rosa <kosolrosa@gmail.com>
2023-12-14 20:44:19 +1100

    Updated shopping cart menu description and prices

--- script.js
+++ script.js
@@ -133,66 +133,66 @@ let basket = [];
 let shopItemsData = [{
   id:"kfc",
   name:"Korean Fried Chicken",
-  price:"14.99",
-  desc:"Crispy Korean-style fried chicken with your choice of flavour, from sweet to spicy.",
+  price:"17.00",
+  desc:"Crispy Korean-style fried chicken with your choice of flavour, from sweet to spicy. Comes with 9 pieces.",
   img:"assets/img/kfc.jpg"
 },
 {
   id:"bulgogi",
   name:"Bulgogi Rice Bowl",
-  price:"4.99",
-  desc:"Crispy Korean-style fried chicken with your choice of flavour, from sweet to spicy.",
+  price:"15.00",
+  desc:"Tender slices of marinated beef grilled to perfection, served over a bed of fluffy rice.",
   img:"assets/img/bulgogi.jpg"
 },
 {
   id:"bibimbap",
   name:"Bibimbap",
-  price:"14.99",
-  desc:"Crispy Korean-style fried chicken with your choice of flavour, from sweet to spicy.",
+  price:"15.00",
+  desc:"A colorful and nutritious Korean dish featuring a mix of seasoned vegetables, rice, and a choice of protein, topped with a fried egg and spicy gochujang sauce.",
   img:"assets/img/bibimbap.jpg"
 },
 {
   id:"tteokbokki",
   name:"Tteokbokki",
-  price:"14.99",
-  desc:"Crispy Korean-style fried chicken with your choice of flavour, from sweet to spicy.",
+  price:"13.00",
+  desc:"Chewy rice cakes immersed in a sweet and spicy gochujang-based sauce, offering a delightful Korean street food experience.",
   img:"assets/img/tteokbokki.jpg"
 },
 {
   id:"eggplant",
-  name:"Korean fried chicken",
-  price:"14.99",
-  desc:"Crispy Korean-style fried chicken with your choice of flavour, from sweet to spicy.",
+  name:"Eggplant and Rice",
+  price:"16.00",
+  desc:"Grilled or stir-fried eggplant paired with seasoned rice, creating a satisfying and flavorful vegetarian option.",
   img:"assets/img/eggplant.jpg"
 },
 {
   id:"noodles",
-  name:"Korean fried chicken",
-  price:"14.99",
-  desc:"Crispy Korean-style fried chicken with your choice of flavour, from sweet to spicy.",
+  name:"Kimchi Soup Noodles",
+  price:"16.00",
+  desc:"A comforting noodle soup infused with the bold flavors of kimchi, providing a spicy and tangy kick.",
   img:"assets/img/noodles.jpg"
 },
 {
   id:"porkbelly",
-  name:"Korean fried chicken",
-  price:"14.99",
-  desc:"Crispy Korean-style fried chicken with your choice of flavour, from sweet to spicy.",
+  name:"Porkbelly Bowl",
+  price:"18.00",
+  desc:"Succulent slices of pork belly, grilled or braised to perfection, served atop a bed of steamed rice.",
   img:"assets/img/porkbelly.jpg"
 },
 {
   id:"jajangmyeon",
-  name:"Korean fried chicken",
-  price:"14.99",
-  desc:"Crispy Korean-style fried chicken with your choice of flavour, from sweet to spicy.",
+  name:"Jajangmyeon",
+  price:"16.00",
+  desc:"Korean-Chinese black bean noodles featuring thick, chewy noodles smothered in a savory and rich black bean sauce.",
   img:"assets/img/jajangmyeon.jpg"
 },
 {
   id:"spicynoodles",
-  name:"Korean fried chicken",
-  price:"14.99",
-  desc:"Crispy Korean-style fried chicken with your choice of flavour, from sweet to spicy.",
+  name:"Spicy Noodles",
+  price:"16.00",
+  desc:"A fiery and tantalizing noodle dish, featuring a blend of spicy sauces and ingredients to ignite the taste buds.",
   img:"assets/img/spicynoodles.jpg"
-},
+}
 ];
 
 console.log(shop);
