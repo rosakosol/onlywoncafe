e4f13f489da4a47a915f679a5914241bc3962ac7
Rosa <kosolrosa@gmail.com>
2023-12-24 15:25:07 +1100

    Finalised checkout page

---
+++ data.js
@@ -0,0 +1,65 @@
+let shopItemsData = [{
+    id:"kfc",
+    name:"Korean Fried Chicken",
+    price:"17.00",
+    desc:"Crispy Korean-style fried chicken with your choice of flavour, from sweet to spicy. Comes with 9 pieces.",
+    img:"assets/img/kfc.jpg"
+  },
+  {
+    id:"bulgogi",
+    name:"Bulgogi Rice Bowl",
+    price:"15.00",
+    desc:"Tender slices of marinated beef grilled to perfection, served over a bed of fluffy rice.",
+    img:"assets/img/bulgogi.jpg"
+  },
+  {
+    id:"bibimbap",
+    name:"Bibimbap",
+    price:"15.00",
+    desc:"A colorful and nutritious Korean dish featuring a mix of seasoned vegetables, rice, and a choice of protein, topped with a fried egg and spicy gochujang sauce.",
+    img:"assets/img/bibimbap.jpg"
+  },
+  {
+    id:"tteokbokki",
+    name:"Tteokbokki",
+    price:"13.00",
+    desc:"Chewy rice cakes immersed in a sweet and spicy gochujang-based sauce, offering a delightful Korean street food experience.",
+    img:"assets/img/tteokbokki.jpg"
+  },
+  {
+    id:"eggplant",
+    name:"Eggplant and Rice",
+    price:"16.00",
+    desc:"Grilled or stir-fried eggplant paired with seasoned rice, creating a satisfying and flavorful vegetarian option.",
+    img:"assets/img/eggplant.jpg"
+  },
+  {
+    id:"noodles",
+    name:"Kimchi Soup Noodles",
+    price:"16.00",
+    desc:"A comforting noodle soup infused with the bold flavors of kimchi, providing a spicy and tangy kick.",
+    img:"assets/img/noodles.jpg"
+  },
+  {
+    id:"porkbelly",
+    name:"Porkbelly Bowl",
+    price:"18.00",
+    desc:"Succulent slices of pork belly, grilled or braised to perfection, served atop a bed of steamed rice.",
+    img:"assets/img/porkbelly.jpg"
+  },
+  {
+    id:"jajangmyeon",
+    name:"Jajangmyeon",
+    price:"16.00",
+    desc:"Korean-Chinese black bean noodles featuring thick, chewy noodles smothered in a savory and rich black bean sauce.",
+    img:"assets/img/jajangmyeon.jpg"
+  },
+  {
+    id:"spicynoodles",
+    name:"Spicy Noodles",
+    price:"16.00",
+    desc:"A fiery and tantalizing noodle dish, featuring a blend of spicy sauces and ingredients to ignite the taste buds.",
+    img:"assets/img/spicynoodles.jpg"
+  }
+  ];
+  
--- script.js
+++ script.js
@@ -130,71 +130,6 @@ let shop = document.getElementById("shop")
 
 let basket = JSON.parse(localStorage.getItem("data")) || [];
 
-let shopItemsData = [{
-  id:"kfc",
-  name:"Korean Fried Chicken",
-  price:"17.00",
-  desc:"Crispy Korean-style fried chicken with your choice of flavour, from sweet to spicy. Comes with 9 pieces.",
-  img:"assets/img/kfc.jpg"
-},
-{
-  id:"bulgogi",
-  name:"Bulgogi Rice Bowl",
-  price:"15.00",
-  desc:"Tender slices of marinated beef grilled to perfection, served over a bed of fluffy rice.",
-  img:"assets/img/bulgogi.jpg"
-},
-{
-  id:"bibimbap",
-  name:"Bibimbap",
-  price:"15.00",
-  desc:"A colorful and nutritious Korean dish featuring a mix of seasoned vegetables, rice, and a choice of protein, topped with a fried egg and spicy gochujang sauce.",
-  img:"assets/img/bibimbap.jpg"
-},
-{
-  id:"tteokbokki",
-  name:"Tteokbokki",
-  price:"13.00",
-  desc:"Chewy rice cakes immersed in a sweet and spicy gochujang-based sauce, offering a delightful Korean street food experience.",
-  img:"assets/img/tteokbokki.jpg"
-},
-{
-  id:"eggplant",
-  name:"Eggplant and Rice",
-  price:"16.00",
-  desc:"Grilled or stir-fried eggplant paired with seasoned rice, creating a satisfying and flavorful vegetarian option.",
-  img:"assets/img/eggplant.jpg"
-},
-{
-  id:"noodles",
-  name:"Kimchi Soup Noodles",
-  price:"16.00",
-  desc:"A comforting noodle soup infused with the bold flavors of kimchi, providing a spicy and tangy kick.",
-  img:"assets/img/noodles.jpg"
-},
-{
-  id:"porkbelly",
-  name:"Porkbelly Bowl",
-  price:"18.00",
-  desc:"Succulent slices of pork belly, grilled or braised to perfection, served atop a bed of steamed rice.",
-  img:"assets/img/porkbelly.jpg"
-},
-{
-  id:"jajangmyeon",
-  name:"Jajangmyeon",
-  price:"16.00",
-  desc:"Korean-Chinese black bean noodles featuring thick, chewy noodles smothered in a savory and rich black bean sauce.",
-  img:"assets/img/jajangmyeon.jpg"
-},
-{
-  id:"spicynoodles",
-  name:"Spicy Noodles",
-  price:"16.00",
-  desc:"A fiery and tantalizing noodle dish, featuring a blend of spicy sauces and ingredients to ignite the taste buds.",
-  img:"assets/img/spicynoodles.jpg"
-}
-];
-
 let generateShop =()=>{
   return (shop.innerHTML = shopItemsData.map((x)=>{
     let {id, name, price, desc, img} = x;
@@ -209,9 +144,8 @@ let generateShop =()=>{
                   <h3>$ ${price}</h3>
                   <div class="quantity-btns">
                     <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
-                    <div id=${id} class="quantity"><h3>${search.item === undefined? 0: search.item}</h3></div>
+                    <div id=${id} class="quantity">${search.item === undefined? 0: search.item}</div>
                     <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
-                    <button type="button" class="btn btn-dark">Add to cart </button>
                   </div>
                 </div>
               </div>
@@ -236,9 +170,7 @@ let increment = (id) => {
   else{
     search.item += 1;
   }
-  // console.log(basket);
   update(selectedItem.id);
-
   localStorage.setItem("data", JSON.stringify(basket));
 };
 
@@ -254,8 +186,6 @@ let decrement = (id)=> {
   }
   update(selectedItem.id);
   basket = basket.filter((x)=> x.item !== 0);
-  // console.log(basket);
-
   localStorage.setItem("data", JSON.stringify(basket));
 };
 
